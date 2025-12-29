bf5091632380e9fc396adfd2c0cfeffde577837c
TomasRomagnoli <romaquilmestomi@gmail.com>
2025-12-29 02:21:15 -0300

    Entidades terminadas

---
+++ Domain/Entities/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string RazonSocial { get; set; } = string.Empty;
+        public string CUIT { get; set; } = string.Empty;
+        public string? Email { get; set; }
+
+        public string? Telefono { get; set; }
+        public int DiasPlazoPago { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; } = null!; //Operador de perdón de nulos, parece nulo ahora pero cuando alguien lo use va a tener un valor
+
+        public ICollection<Factura> Facturas { get; set; } = new List<Factura>();
+
+
+    }
+}
---
+++ Domain/Entities/Factura.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public string NumeroComprobante { get; set; } = string.Empty;
+
+        public DateTime FechaEmision { get; set; }
+        public DateTime? FechaVencimiento { get; set; }
+        public DateTime? FechaDePago { get; set; }
+        public decimal ImporteTotal { get; set; }
+        public string? Observaciones { get; set; }
+        
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; } = null!;
+
+        public decimal CalcularIva() => ImporteTotal - (ImporteTotal / 1.21m);
+
+        public decimal CalcularNeto() => ImporteTotal / 1.21m;
+
+        public string Estado
+        {
+            get
+            {
+                if (FechaDePago.HasValue) return "Pagada";
+                if (FechaVencimiento.HasValue && FechaVencimiento < DateTime.Now) return "Vencida";
+                return "Pendiente";
+            }
+        }
+
+    }
+}
