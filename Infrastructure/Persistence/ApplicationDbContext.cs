20b6e0c0226bd1f05a8a5ee5b19dcc529b12fab5
TomasRomagnoli <romaquilmestomi@gmail.com>
2026-01-08 12:17:57 -0300

    DbContext, configurations y migracion

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; } = null!;
+        public DbSet<Cliente> Clientes { get; set; } = null!;
+
+        public DbSet<Factura> Facturas { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Cliente>()
+                .HasIndex(c => c.CUIT)
+                .IsUnique();
+
+            modelBuilder.Entity<Factura>()
+                .Property(f => f.ImporteTotal)
+                .HasPrecision(18, 2);
+        }
+    }
+}
